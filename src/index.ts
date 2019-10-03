c3d7d377559ebf963b3099aa895bb0b2bfdbd04c
Kristian F Jones <me@kristianjones.xyz>
2019-10-03 17:48:16 -0500

    feat: More work.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 // src/index.ts
+import { getInput } from '@actions/core';
+import { context, GitHub } from '@actions/github';
+
+const githubToken = getInput('githubToken');
+const octokit = new GitHub(githubToken);
+
 async function runAction(): Promise<void> {
-  console.log(process.env);
+  console.log(context);
+  // octokit.pulls.get()
+
+  // octokit.repos.createDeployment({ ref })
 }
 
 runAction();
